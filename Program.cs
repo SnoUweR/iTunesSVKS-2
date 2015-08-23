fdc074419897f1f50bb43f0bc1e9e2fdad9266ea
SnoUweR <snouwer@gmail.com>
2015-08-24 02:29:06 +0700

    для дебага

--- Program.cs
+++ Program.cs
@@ -7,15 +7,22 @@ namespace iTunesSVKS_2
 {
     static class Program
     {
+        static int mainThreadId;
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            mainThreadId = System.Threading.Thread.CurrentThread.ManagedThreadId;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        public static bool IsMainThread
+        {
+            get { return System.Threading.Thread.CurrentThread.ManagedThreadId == mainThreadId; }
         }
     }
 }
